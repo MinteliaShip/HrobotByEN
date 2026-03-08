d7849bcca460904a45db9ba089223f4adec42343
Smasa <hisagoma829@gmail.com>
2026-03-08 19:17:27 +0900

    ライブラリ補助作成

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,34 @@
 #include <Arduino.h>
 #include <servoICS.h>
 
+#include "PS4Controller_support.h"
+#include "esp_timer.h"
+
 // put function declarations here:
 int myFunction(int, int);
 
+servoICS::Servo servo1;
+PS4Controller_support Dualshock4;
+
+
+int executionCycil( double cycilTime, double offsetTime, double fps, void (*p_func)(double)){
+  int64_t start_time = esp_timer_get_time();
+  int64_t intervalTime = int64_t(1000000.0/fps);
+  int64_t nextTime = start_time + intervalTime;
+
+  while(true){
+    nextTime += intervalTime;
+    p_func(double(esp_timer_get_time() - start_time)*0.000001);
+    while(esp_timer_get_time() - start_time < nextTime)delay(1);
+    if(esp_timer_get_time() - start_time >= int64_t(cycilTime*1000000))break;
+  }
+}
+
 void setup() {
   // put your setup code here, to run once:
+  Dualshock4.begin("12:04:00:00:05:05");
+  Dualshock4.update();
+  
   int result = myFunction(2, 3);
 }
 
